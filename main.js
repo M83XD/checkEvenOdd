9ba66ed8967a673e5e44131f060a943babcd3c0a
M83 <75308710+M83XD@users.noreply.github.com>
2023-02-08 23:59:51 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+<script>
+   function checkEvenOdd() {
+      var num = parseInt(prompt("Enter a number:"));
+      if (num % 2 == 0) {
+         alert(num + " is an even number.");
+      } else {
+         alert(num + " is an odd number.");
+      }
+   }
+</script>
+
+<button onclick="checkEvenOdd()">Click me</button>
